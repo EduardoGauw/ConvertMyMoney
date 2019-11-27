ff1d1d182799072341eea33fe31f92d3bde20415
Eduardo Gauw <edugauw@gmail.com>
2019-11-26 22:02:15 -0300

    No comments.

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/cotacao', (req, res) => {
-    // console.log(req) para ver onde está vindo a cotação e a quantidade: vem na query
-    // console.log(req.query) descobre o que vem na query para poder saber o que vai extrair
     const {cotacao, quantidade} = req.query
     if (cotacao && quantidade) {
         const conversao = convert.convert(cotacao, quantidade)
-        res.render('cotacao', { // isso é a pagia com a resposta para essa conta e, baixo o que vai mostrar
+        res.render('cotacao', { 
             error: false,
             cotacao: convert.toMoney(cotacao),
             quantidade: convert.toMoney(quantidade),
